94c098f8dc22580982d270b5c5464d3a89a32fb9
_LittleGiant_ <48399282+hswhsw610@users.noreply.github.com>
2022-09-30 22:10:57 +0900

    Update execute-buttons.ts

--- src/events/interactionCreate/execute-buttons.ts
+++ src/events/interactionCreate/execute-buttons.ts
@@ -1,15 +1,21 @@
 import { createInteractionCreateEventListener } from "./index";
 
 export default createInteractionCreateEventListener((interaction) => {
+
     if (!interaction.isButton()) return;
 
-    const filter = (i: any) => i.customId === "selectroles_male";
+    const filter = (i: any) => i.customId.startsWith("selectroles");
+
     const collector = interaction.channel?.createMessageComponentCollector({ filter });
+
     if (!collector) return;
 
     collector.on("collect", async (i) => {
+
         await i.editReply({ content: "test", ephemeral: true });
+
     });
 
     collector.on("end", (collected) => console.log(`Collected ${collected.size} items`));
+
 });
