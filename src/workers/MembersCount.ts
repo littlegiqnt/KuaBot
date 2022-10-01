8880b7604b6b34bd879d1edbbed91f1c3fb4e234
_LittleGiant_ <48399282+hswhsw610@users.noreply.github.com>
2022-10-01 21:53:09 +0900

    Update MembersCount.ts

--- src/workers/MembersCount.ts
+++ src/workers/MembersCount.ts
@@ -1,9 +1,9 @@
 import type { Client, VoiceChannel } from "discord.js";
 
 export default async (client: Client) => {
-    const guild = await client.guilds.fetch("1023072472712609873");
-    if (!guild) return;
     setInterval(async function () {
+        const guild = await client.guilds.fetch("1023072472712609873");
+        if (!guild) return;
         var memberCountChannel: VoiceChannel = client.channels.cache.get(
             "1023190822692323369"
         ) as VoiceChannel;
