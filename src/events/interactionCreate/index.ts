27eebd1d3d341d10428d846f974124c0bf18c5b4
hswhsw610 <hswhsw610@gmail.com>
2022-10-01 14:03:26 +0900

    Update roles select buttons

--- src/events/interactionCreate/index.ts
+++ src/events/interactionCreate/index.ts
@@ -2,7 +2,7 @@ import { createEventListenerFactory } from '../../structure/EventListener'
 export const createInteractionCreateEventListener = createEventListenerFactory('interactionCreate')
 
 import executeCommands from "./execute-commands";
-import executeButtons from "./execute-buttons";
+import executeButtons from "./ExecuteButtons";
 
 export default [
 	executeCommands,
