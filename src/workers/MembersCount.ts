286aec35c8dc05a8ffc8d6a1e5a6accd5aa3f524
hswhsw610 <hswhsw610@gmail.com>
2022-10-02 12:54:56 +0900

    Update

--- src/workers/MembersCount.ts
+++ src/workers/MembersCount.ts
@@ -4,9 +4,8 @@ export default async (client: Client) => {
     setInterval(async function () {
         const guild = await client.guilds.fetch("1023072472712609873");
         if (!guild) return;
-        var memberCountChannel: VoiceChannel = client.channels.cache.get(
-            "1023190822692323369"
-        ) as VoiceChannel;
+        await guild.fetch();
+        var memberCountChannel: VoiceChannel = client.channels.cache.get("1023190822692323369") as VoiceChannel;
         memberCountChannel.setName(`『👤｜${guild.memberCount}명』`);
     }, 10000);
 };
