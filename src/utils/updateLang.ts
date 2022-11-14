93dbb4acb832d8cedf1001c705dc4e497e404b29
hswhsw610 <hswhsw610@gmail.com>
2022-11-14 09:58:22 +0900

    외국인 역할 자동 업데이트

--- src/utils/updateLang.ts
+++ src/utils/updateLang.ts
@@ -1,18 +1,28 @@
 import type { MessageComponentInteraction, SelectMenuInteraction } from "discord.js";
-import { Locale } from "discord.js";
+import { GuildMember, Locale } from "discord.js";
 import dbManager from "structure/DBManager";
+import rolesManager from "structure/RolesManager";
 
 export default async (interaction: MessageComponentInteraction|SelectMenuInteraction, lang: Locale) => {
+    const { member } = interaction;
+    if (!(member instanceof GuildMember)) return;
     const user = await dbManager.loadUser(interaction.user.id);
     user.lang = lang;
-    await user.save();
     switch (lang) {
         case Locale.Korean: {
+            await Promise.all([
+                user.save(),
+                member.roles.remove(rolesManager.get("foreign")!),
+            ]);
             interaction.editReply("언어를 한국어로 선택해 드렸어요!");
             break;
         }
         case Locale.EnglishGB:
         case Locale.EnglishUS: {
+            await Promise.all([
+                user.save(),
+                member.roles.add(rolesManager.get("foreign")!),
+            ]);
             interaction.editReply("I've set your language to English");
             break;
         }
