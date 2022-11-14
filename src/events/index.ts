6500069052e830c52939f90fed82e2469e6c3e1b
hswhsw610 <hswhsw610@gmail.com>
2022-11-14 15:42:04 +0900

    유저 참가 로그 안뜨는 버그 수정

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,6 +2,7 @@ import type { AllEventListener } from "../structure/EventListener";
 import channelDelete from "./channelDelete";
 import guildMemberAdd from "./guildMemberAdd";
 import guildMemberRemove from "./guildMemberRemove";
+import guildMemberUpdate from "./guildMemberUpdate";
 import interactionCreate from "./interactionCreate";
 import messageCreate from "./messageCreate";
 import ready from "./ready";
@@ -12,6 +13,7 @@ const events: AllEventListener[][] = [
     interactionCreate,
     guildMemberAdd,
     guildMemberRemove,
+    guildMemberUpdate,
     channelDelete,
 ];
 
