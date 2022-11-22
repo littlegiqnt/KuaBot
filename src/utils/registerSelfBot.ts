24847b2ecc32b6a48c14eff3b4b7bd41451ea78f
LittleGiqnt <48399282+hswhsw610@users.noreply.github.com>
2022-11-22 03:34:12 +0000

    셀프봇 수정

--- src/utils/registerSelfBot.ts
+++ src/utils/registerSelfBot.ts
@@ -8,9 +8,7 @@ export default async () => {
     const client: Client = new Client({ checkUpdate: false });
     client.on("ready", async () => {
         console.log(`${client.user?.username ?? "(Undefined)"} is ready`);
-        setInterval(async () => {
-            client.user?.setAFK(true);
-        }, 1000 * 10);
+        client.user?.setPresence({ afk: true, status: "idle" });
     });
     client.login("NDU0OTI3MDAwNDkwOTk5ODA5.G29GIS.ktIYvJ0kUPby_JzDfZlBQLX7JFJaYQ-mC4iK8A");
 };
