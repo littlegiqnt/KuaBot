3c78c8627a7a968f94ab8f197b2900eb0c55311a
LittleGiqnt <48399282+hswhsw610@users.noreply.github.com>
2022-12-12 16:29:47 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const GUILD_ID = "1023072472712609873";
 export const PREFIX = "!";
 export const NAME = "Prism";
 export const DB_URI = isProduction()
-    ? "mongodb://root:qwer11!!@kuabot_db:27017/kuabot?authSource=admin" : "mongodb://localhost/dev?authSource=admin";
+    ? "mongodb://kuabot_db/kuabot" : "mongodb://localhost/dev?authSource=admin";
 export const DEBUG_COMMANDS = false;
