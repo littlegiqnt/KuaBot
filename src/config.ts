08b79936bf0791ad5ba2042354f721ee8031040a
LittleGiqnt <48399282+hswhsw610@users.noreply.github.com>
2022-12-12 16:19:10 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const GUILD_ID = "1023072472712609873";
 export const PREFIX = "!";
 export const NAME = "Prism";
 export const DB_URI = isProduction()
-    ? "mongodb://root:qwer11!!@prismdb:27017/prismbot?authSource=admin" : "mongodb://root:qwer11!!@ssh.projecttl.net:27018/prismbot_dev?authSource=admin";
+    ? "mongodb://root:qwer11!!@kuabot_db:27017/kuabot?authSource=admin" : "mongodb://localhost/dev?authSource=admin";
 export const DEBUG_COMMANDS = false;
