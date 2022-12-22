522b544c23495f304f239710235ffcc5b90fcd7c
LittleGiqnt <48399282+hswhsw610@users.noreply.github.com>
2022-12-22 07:40:25 +0000

    fix: database uri

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const GUILD_ID = "1023072472712609873";
 export const PREFIX = "!";
 export const NAME = "Prism";
-export const DB_URI = "mongodb://kuabot_db/kuabot";
+export const DB_URI = "mongodb://kua_db/kuabot";
 export const DEBUG_COMMANDS = false;
