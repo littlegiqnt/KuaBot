72d6d514dd6d4948d5ff4c30ff4b10e313cd3959
LittleGiqnt <48399282+hswhsw610@users.noreply.github.com>
2022-12-31 13:52:20 +0900

    fix: selfbot notification bug

--- src/utils/registerSelfBot.ts
+++ src/utils/registerSelfBot.ts
@@ -5,11 +5,11 @@ export default async () => {
     if (!isProduction()) {
         return;
     }
-    const client: Client = new Client({ checkUpdate: false, autoRedeemNitro: true });
+    const client: Client = new Client({ autoRedeemNitro: true });
     client.on("ready", async () => {
         console.log(`${client.user?.username ?? "(Undefined)"} is ready`);
-        client.user?.setAFK(true);
         client.user?.setStatus("idle");
+        client.user?.setAFK(true);
     });
     client.login(process.env.USER_TOKEN);
 };
